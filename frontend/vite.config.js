1c5055b2f15f4283e4e1b3e04791a497f87dd99f
hyunaan-ship-it <hyuna.an@lgepartner.com>
2026-05-27 14:18:34 +0900

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/student-list/',
+  base: '/list-site/',
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
+        target: 'http://localhost:5000'
       }
     }
   }
 })
-
